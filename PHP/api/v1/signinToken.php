5e43c050aae525b5e8f713de9006b384f23fa804
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-01-01 18:48:46 +0200

    pre-prepare for signIn

---
+++ PHP/api/v1/signinToken.php
@@ -0,0 +1,18 @@
+<?php 
+
+include("../../sql/select.php");
+
+function signIn($conn, $email, $password)
+{
+    $sql = "SELECT * FROM Users WHERE user_email = '$email' AND user_password = '$password'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result -> fetch_assoc();
+        return $row;
+    } else {
+        return false;
+    }
+}
+
+
+?>

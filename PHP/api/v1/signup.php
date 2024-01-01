58b0835d13f6cc8a8773f8ba90eba42b51e37e9f
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-01-01 18:13:19 +0200

    signup end point php

---
+++ PHP/api/v1/signup.php
@@ -0,0 +1,35 @@
+<?php
+
+
+include("../../sql/insert.php");
+
+// print all the POST parameters
+
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$date = $_POST["date"];
+$balance = $_POST["balance"];
+$type = $_POST["type"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+$city = $_POST["city"];
+$country = $_POST["country"];
+$gender = $_POST["gender"];
+
+
+
+
+$result = insertIntoUsers(conn, $firstname, $lastname, $date, $balance, $type, $password, $email, $city, $country, $gender);
+
+
+// $data = array('message' => $result ? 'user created successfully' : "Error creating user", 'result' => $result ? 200 : 404);
+
+// Convert the array to JSON
+// $json = json_encode($data);
+
+// Output the JSON
+
+// echo $json;
+
+
+?>

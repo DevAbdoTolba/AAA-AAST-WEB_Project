ddf251627f034b6d309616bfc4d6b112dcd8e0d0
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-01-02 14:57:42 +0200

    alert error from backend

--- PHP/api/v1/signup.php
+++ PHP/api/v1/signup.php
@@ -8,18 +8,37 @@ include("../../sql/insert.php");
 $firstname = $_POST["firstname"];
 $lastname = $_POST["lastname"];
 $date = $_POST["date"];
-$balance = $_POST["balance"];
-$type = $_POST["type"];
 $password = $_POST["password"];
 $email = $_POST["email"];
 $city = $_POST["city"];
 $country = $_POST["country"];
+
 $gender = $_POST["gender"];
+$balance = $_POST["balance"];
+$type = $_POST["type"];
 
 
-$token = $email . $firstname . $lastname . $password;
+// if any value is empty echo the first empty value's key
 
-$result = insertIntoUsers(conn, $firstname, $lastname, $date, $balance, $token, $type, $password, $email, $city, $country, $gender);
+$flag = true;
+
+
+
+foreach ($_POST as $key => $value) {
+    if (!(strlen($value) > 0)) {
+        echo json_encode(array("message" => "Error: " . $key . " is empty", "status" => 409));
+        $flag = false;
+        break;
+    }
+}
+
+if ($flag) {
+
+
+    $token = $email . $firstname . $lastname . $password;
+
+    $result = insertIntoUsers(conn, $firstname, $lastname, $date, $balance, $token, $type, $password, $email, $city, $country, $gender);
+}
 
 
 // $data = array('message' => $result ? 'user created successfully' : "Error creating user", 'result' => $result ? 200 : 404);
--- PHP/section/sec.php
+++ PHP/section/sec.php
@@ -9,6 +9,10 @@
 
 <body>
 
+<?php 
+?>
+
+
     <form method="POST" action="res.php"><input type="text" name="username"><input type="password" name="password"><input type="submit" name="submit">
     </form>
     

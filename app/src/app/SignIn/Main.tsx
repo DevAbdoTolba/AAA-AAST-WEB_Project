c577a7f68d6a069117175dfe8cb8dbee0ebbeef3
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-01-02 16:11:30 +0200

    front sign in

--- app/src/app/SignIn/Main.tsx
+++ app/src/app/SignIn/Main.tsx
@@ -3,12 +3,34 @@
 import React from "react";
 import "./style.css";
 import Link from "next/link";
-export default function main() {
+export default function Main() {
+
+  const [data, setData] = React.useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  console.log(data);
+  
+    fetch(`http://localhost:3000/api/client/login?email=${data.email}&password=${data.password}`).then(data => data.json()).then(res=> {
+      console.log(res);
+      if(res.status === 200) {
+        localStorage.setItem("token", res.data.user_token)
+        localStorage.setItem("firstname", JSON.stringify(res.data.user_firstname))
+        localStorage.setItem("balance", res.data.user_balance)
+        window.location.href = "/"
+      }
+    })
+  }
   return (
     <>
       <main>
         <div className="box1">
-          <form action={"home"}>
+          <form onSubmit={handleSubmit}>
             <div>
               <h2 style={{ color: "#7c70a4" }}>Sign In</h2>
               <p>&nbsp;</p>
@@ -21,6 +43,7 @@ export default function main() {
                   type="text"
                   name="email"
                   placeholder="email"
+                  onChange={handleChange}
                 />
               </div>
               <div className="twoInRow">
@@ -29,8 +52,9 @@ export default function main() {
                   <input
                     id="Password"
                     type="text"
-                    name="Password"
+                    name="password"
                     placeholder="Password"
+                    onChange={handleChange}
                   />
                 </div>
               </div>

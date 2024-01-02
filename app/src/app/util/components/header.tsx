9268bc6b435a841a4d6ed1d117562c576c4c12ad
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-01-02 17:23:47 +0200

    login logic in front session token done

--- app/src/app/util/components/header.tsx
+++ app/src/app/util/components/header.tsx
@@ -1,16 +1,49 @@
 "use client";
 
 import * as React from "react";
+import { FirstNameContext } from "@/app/ThemeComponent"; // replace './Layout' with the actual path of your Layout component
+
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import { ButtonGroup, Grid, IconButton } from "@mui/material";
+import {
+  Avatar,
+  ButtonGroup,
+  Grid,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import Link from "next/link";
 import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
 export default function Header() {
+  const token = React.useContext(FirstNameContext);
+
+  const [name, setName] = React.useState("");
+
+  const [isLogin, setIsLogin] = React.useState(false);
+  React.useEffect(() => {
+    console.log(token);
+
+    if (token == "" || token == null || token == undefined) {
+      return;
+    } else {
+      console.log("logged in");
+
+      setIsLogin(true);
+    }
+  }, [token]);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Box sx={{ flexGrow: 1 }} id="header">
       <AppBar
@@ -154,13 +187,30 @@ export default function Header() {
               <IconButton>
                 <SearchIcon />
               </IconButton>
-              <IconButton
-                onClick={() => {
-                  window.location.href = "/SignIn";
-                }}
-              >
-                <PersonOutlineOutlinedIcon />
-              </IconButton>
+              {isLogin ? (
+                <IconButton onClick={handleClick}>
+                  <Avatar
+                    sx={{
+                      bgcolor: "Main.primary",
+                      color: "Main.white",
+                      fontWeight: 700,
+                    }}
+                    aria-controls={open ? "account-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? "true" : undefined}
+                  >
+                    {token[0].toUpperCase()}
+                  </Avatar>
+                </IconButton>
+              ) : (
+                <IconButton
+                  onClick={() => {
+                    window.location.href = "/SignIn";
+                  }}
+                >
+                  <PersonOutlineOutlinedIcon />
+                </IconButton>
+              )}
               <IconButton
                 onClick={() => {
                   window.location.href = "/cart";
@@ -172,6 +222,27 @@ export default function Header() {
           </Grid>
         </Grid>
       </AppBar>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem
+          onClick={() => {
+            localStorage.removeItem("token");
+            localStorage.removeItem("firstname");
+            localStorage.removeItem("balance");
+            handleClose();
+            window.location.reload();
+          }}
+        >
+          Logout
+        </MenuItem>
+      </Menu>
     </Box>
   );
 }

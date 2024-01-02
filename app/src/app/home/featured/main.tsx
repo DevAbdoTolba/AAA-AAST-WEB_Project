d5e1739c1c152976434ebc88d3d3137936163cb7
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-01-02 19:56:26 +0200

    front done products on N

--- app/src/app/home/featured/main.tsx
+++ app/src/app/home/featured/main.tsx
@@ -1,76 +1,34 @@
+"use client";
+
+import React from "react";
 import Card from "@/app/util/components/Card";
 import { Typography, Box, Button } from "@mui/material";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { useEffect } from "react";
 const MotionBox = motion(Box);
 
 export default function Main() {
-  const MockData = [
+  const [data, setData] = React.useState([
     {
-      favorite: false,
-      image: "chair.png",
-      state: "Discount",
-      description: "Very good Chair",
-      price: 100,
+      product_image: "",
+      product_state: "",
+      product_description: "",
+      product_price: 0,
       inCart: false,
-    },
-    {
-      favorite: true,
-      image: "table.png",
-      state: "Sold Out",
-      description: "Very good Table",
-      price: 200,
-      inCart: true,
-    },
-    {
       favorite: false,
-      image: "lambs.png",
-      state: "Discount",
-      description: "Very good lambs",
-      price: 300,
-      inCart: false,
     },
-    {
-      favorite: true,
-      image: "coach.png",
-      state: "Sold Out",
-      description: "Very good coach",
-      price: 400,
-      inCart: true,
-    },
-    {
-      favorite: false,
-      image: "chair.png",
-      state: "New",
-      description: "Very good chair",
-      price: 500,
-      inCart: false,
-    },
-    {
-      favorite: true,
-      image: "lambs.png",
-      state: "Sold Out",
-      description: "Very good lambs",
-      price: 600,
-      inCart: true,
-    },
-    {
-      favorite: false,
-      image: "coach.png",
-      state: "New",
-      description: "Very good coach",
-      price: 700,
-      inCart: false,
-    },
-    {
-      favorite: true,
-      image: "table.png",
-      state: "Sold Out",
-      description: "Very good Table",
-      price: 800,
-      inCart: true,
-    },
-  ];
+  ]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/products/route?n=8")
+      .then((data) => data.json())
+      .then((res) => {
+        console.log(res);
+
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <>
@@ -78,37 +36,38 @@ export default function Main() {
         width={"100%"}
         display={"grid"}
         // grid 4 in the first row then 4 in the second row
-       
+
         gap={2}
         textAlign={"center"}
         bgcolor={"#fff"}
         sx={{
           gridTemplateColumns: {
-            xs : "1fr",
-            sm : "1fr 1fr 1fr 1fr",
-          }
+            xs: "1fr",
+            sm: "1fr 1fr 1fr 1fr",
+          },
         }}
       >
-        {MockData.map((item, i) => (
+        {data.map((item, i) => (
           <Card
             key={i}
-            favorite={item.favorite}
-            image={item.image}
-            state={item.state}
-            description={item.description}
-            price={item.price}
-            inCart={item.inCart}
+            favorite={item?.favorite || false}
+            image={item?.product_image}
+            state={item?.product_state}
+            description={item?.product_description}
+            price={item?.product_price}
+            inCart={item?.inCart || false}
           />
         ))}
       </MotionBox>
-      <Button className={"outlined"} 
+      <Button
+        className={"outlined"}
         sx={{
           px: 8,
           py: 1.5,
           mb: 5,
         }}
-        onClick={()=>{
-          window.location.href = "products"
+        onClick={() => {
+          window.location.href = "products";
         }}
       >
         More Products

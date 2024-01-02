c587a692fe477187b8087196a2017f9630f86c26
Abdo Tolba <DevAbdoTolba@gmail.com>
2024-01-02 19:26:10 +0200

    php get products of N

---
+++ PHP/api/v1/products.php
@@ -0,0 +1,18 @@
+<?php 
+
+include("../../sql/select.php");
+ 
+
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+    $numOfRows = $_GET["numOfRows"];
+    $row = getProducts(conn , $numOfRows);
+    if($row > 0)
+    echo json_encode(array("data" => $row, "status" => 200));
+    else{
+        echo json_encode(array("message" => "Error:No Products available" , "status" => 404));
+    }
+}
+
+?>
+
+
